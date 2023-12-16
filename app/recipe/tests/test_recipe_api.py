365b3b62ea7d8cf68ff2275a4fb2af5123856ca2
sagarkhatridk <sagarkhatridk@gmail.com>
2023-12-16 17:16:03 +0530

    linting error solved for Recipe

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -59,7 +59,6 @@ class PublicRecipeAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-
 class PrivateRecipeAPITests(TestCase):
     """Test authenticated API requests."""
 
